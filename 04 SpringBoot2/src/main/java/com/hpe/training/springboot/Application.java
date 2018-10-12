7d2e50acec3f557f34b04f3dd819ae0e48519e3d
kayartaya.vinod <kayartaya.vinod@gmail.com>
2018-10-12 16:12:52 +0530

    🇮🇳

--- 04 SpringBoot2/src/main/java/com/hpe/training/springboot/Application.java
+++ 04 SpringBoot2/src/main/java/com/hpe/training/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @SpringBootApplication
 public class Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
